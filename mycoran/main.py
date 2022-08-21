3ffc4c920bc23316bae4e7a4e8f7daae8bf7244f
ousinlepro <88011016+ousinlepro@users.noreply.github.com>
2022-08-21 11:27:03 +0000

    Add files via upload

--- mycoran/main.py
+++ mycoran/main.py
@@ -3,29 +3,44 @@
 # https://hci-lab.github.io/PyQuran-Private/analysis_tools/
 import pyquran as q
 from time import sleep
-"""from bs4 import BeautifulSoup
+import re
 
-var = q.quran.get_verse(sura_number=31, verse_number=6)
-
-def fix():
-    page = './index.html'
-    soup = BeautifulSoup(page, 'html.parser')
-    content = soup.find("span", {"id": "py-14dc108c-a2cb-4704-ddf0-444b92c01600-2"})
-    content.string = ""
-
-"""
-
-
-def myverse(*args, **kwargs):
+def mono_verset(*args, **kwargs):
     inp = Element('output')
     mysura = int(Element('sura').value)
-    myverse = int(Element('verse').value)
-    verset = q.quran.get_verse(sura_number=mysura, verse_number=myverse, with_tashkeel=True)
+    verset = int(Element('verse').value)
+    verset = q.quran.get_verse(sura_number=mysura, verse_number=verset, with_tashkeel=True)
     inp.write(f"{verset}")
 
-
-def mysura(*args, **kwargs):
+def multi_verset(*args, **kwargs):
     inp = Element('output')
-    mysura = int(Element('sura').value)
-    sourate = q.quran.get_sura(mysura, with_tashkeel=True)
-    inp.write(sourate)
+    verset = Element('verse').value
+
+    num_versets= verset
+
+    num_versets1 = re.findall(r"\d+:", num_versets)
+    num_versets1 = num_versets1[0]
+    num_versets1 = num_versets1.replace(':','')
+    num_versets1 = int(num_versets1)
+
+    num_versets2 = re.findall(r":\d+", num_versets)
+    num_versets2 = num_versets2[0]
+    num_versets2 = num_versets2.replace(':','')
+    num_versets2 = int(num_versets2)
+
+    for i in range(num_versets1,num_versets2):
+        sourate = int(Element('sura').value)
+        versets = q.quran.get_verse(sura_number=sourate, verse_number=i, with_tashkeel=True)
+    inp.write(versets)
+
+def myverse(*args, **kwargs):
+    verset = Element('verse').value
+    if ':' in verset:
+        multi_verset()
+    else:
+        mono_verset()
+
+
+
+
+
---
+++ poid-mystique/main.py
@@ -0,0 +1,51 @@
+
+l1 = 'ا'
+l2 = 'ب'
+l3 = 'ج'
+l4 = 'د'
+l5 = 'ه'
+l6 = 'و'
+l7 = 'ز'
+l8 = 'ح'
+l9 = 'ط'
+l10 = 'ي'
+l11 = 'ك'
+l12 = 'ل'
+l13 = 'م'
+l14 = 'ن'
+l15 = 'ص'
+l16 = 'ع'
+l17 = 'ف'
+l18 = 'ض'
+l19 = 'ق'
+l20 = 'ر'
+l21 = 'س'
+l22 = 'ت'
+l23 = 'ث'
+l24 = 'خ'
+l25 = 'ذ'
+l26 = 'ظ'
+l27 = 'غ'
+l28 = 'ش'
+
+l29 = 'ة'
+l30 = 'ء'
+
+
+sum_dict = {l1 : 1, l2 : 2, l3 : 3, l4 : 4,  
+            l5 : 5, l6 : 6, l7 : 7, l8 : 8,
+            l9 : 9, l10 : 10, l11 : 20, l12 : 30,
+            l13 : 40, l14 : 50, l15 : 60, l16 : 70,
+            l17 : 80, l18 : 90, l19 : 100, l20 : 200,
+            l21 : 300, l22 : 400, l23 : 500, l24 : 600, 
+            l25 : 700, l26 : 800, l27 : 900, l28 : 1000,
+            l29 : 5, l30:1} 
+def pm(*args, **kwargs):  
+    entry = Element('search').value
+    opt = Element('output')
+    opt2 = Element('valeur-num')
+    res = sum(sum_dict[ele] for ele in entry.replace(' ', '')) 
+    opt.write(f"Sa valeur numerique est de: ")
+    opt2.write(f"{str(res)}")
+    
+
