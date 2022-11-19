fb5a8247d778bf720e81782340da7e7056360534
ousinlepro <ndioneousseynou585@gmail.com>
2022-11-19 21:50:39 +0000

    main

--- note/moyenne.py
+++ note/moyenne.py
@@ -4,7 +4,10 @@ import csv
 import re
 from urllib.request import urlopen
 import requests
+import colorama
+from colorama import Fore
 
+colorama.init(autoreset=True)
 # dev
 
 with open('notes-db.csv',encoding='utf-8') as f:
@@ -28,7 +31,7 @@ for line in file_content:
 
     list_notes.append(note)
 
-    print(line[0],note)
+    print(line[0],note)  # exp: Matiere  17
     
 
 total = sum(list_notes)
@@ -40,8 +43,13 @@ m = mean(list_notes) # la moyenne
 
 prc = (total * 100) / pts_t # la pourcentages de points
 
+if m > 16:
+    m_color = Fore.GREEN
+else:
+    m_color = Fore.RED
+    
 print()
-print(f'[+] moyenne: {m}')
+print(f'[+] moyenne: {m_color}{m}')
 print(f'[+] meilleure note: {max(list_notes)}')
 print(f'[+] plus petite note: {min(list_notes)}')
 print(f'[+] {total} / {pts_t} points')
